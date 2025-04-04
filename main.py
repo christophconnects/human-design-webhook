8a5c676b0b9a44090e837b1fdd9f0a6d522c2838
christophconnects <christophconnects@gmail.com>
2025-04-04 10:56:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '✅ Webhook läuft auf Render!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print("✅ Webhook wurde aufgerufen!")
+    try:
+        data = request.get_json(force=True)
+        print(json.dumps(data, indent=2))
+    except Exception as e:
+        print("❌ Fehler beim JSON-Verarbeiten:", str(e))
+    return "OK", 200
+
+app.run(host="0.0.0.0", port=10000)
 
